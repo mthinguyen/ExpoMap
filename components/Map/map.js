25f0eed3f0742bf7f8529c137fef11a772af564f
Thi Nguyen <dnguyen209@my.bcit.ca>
2023-01-07 18:41:59 -0800

    Circle Marker

--- components/Map/map.js
+++ components/Map/map.js
@@ -8,11 +8,18 @@ import { MapContainer, TileLayer, useMap, Marker, Popup, Polyline, CircleMarker 
 
 export default function Map() {
 
-    const position = [49.28594, -123.11129]
+    const position = [49.28594, -123.11129];
+    const blackOption = { color: 'black'};
+    const redOption = { color: 'red'};
+
+    const polyline = [
+        [49.28594, -123.11129],
+        [49.28250, -123.11865],
+        [49.27450, -123.12189]
+    ]
 
     return (
-        <MapContainer className={style.map} center={position} zoom={14} scrollWheelZoom={true}
-        >f
+        <MapContainer className={style.map} center={position} zoom={14} scrollWheelZoom={true}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -22,8 +29,15 @@ export default function Map() {
                 <Popup>
                     Skytrain: <br />Waterfront Station
                 </Popup>
-
             </Marker>
+
+            <Polyline pathOptions={blackOption} positions={polyline} />
+            
+            <CircleMarker center={[49.27450, -123.12189]} pathOptions={redOption} radius={20}>
+                <Popup>You've arrived at your destination!</Popup>
+            </CircleMarker>
+
+
         </MapContainer>
     )
 
