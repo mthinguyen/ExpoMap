d377227abc6fc32919de6ba1152728bc26cb4ec5
Thi Nguyen <dnguyen209@my.bcit.ca>
2023-01-07 17:45:55 -0800

    Set up

---
+++ components/Map/map.js
@@ -0,0 +1,30 @@
+import 'leaflet/distleaflet.css'
+import 'leaflet-defaulticon-compatibility'
+import 'leaflet-defaulticon-compatibility/dist/leaflet-leafletdefaulticon-compatibility.cssf'
+import style from '../../styles/Home.module.css'
+
+
+import { MapContainer, TileLayer, useMap, Marker, Popup, Polyline, CircleMarker } from 'react-leaflet'
+
+export default function Map() {
+
+const position = [49.28594, -123.11129]
+
+    return (
+        <MapContainer>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            <Marker position={position}>
+            <Popup>
+                Skytrain. <br/> Waterfront.
+            </Popup>
+            
+
+            </Marker>
+        </MapContainer>
+    )
+
+}
