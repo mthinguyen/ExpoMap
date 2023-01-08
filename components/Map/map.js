2d218dc3ca964ee3db26c11fd01d092bf0c66120
Thi Nguyen <dnguyen209@my.bcit.ca>
2023-01-07 18:22:30 -0800

    Pin Waterfront Station

--- components/Map/map.js
+++ components/Map/map.js
@@ -1,6 +1,6 @@
-import 'leaflet/distleaflet.css'
+import 'leaflet/dist/leaflet.css'
 import 'leaflet-defaulticon-compatibility'
-import 'leaflet-defaulticon-compatibility/dist/leaflet-leafletdefaulticon-compatibility.cssf'
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css'
 import style from '../../styles/Home.module.css'
 
 
@@ -8,20 +8,20 @@ import { MapContainer, TileLayer, useMap, Marker, Popup, Polyline, CircleMarker 
 
 export default function Map() {
 
-const position = [49.28594, -123.11129]
+    const position = [49.28594, -123.11129]
 
     return (
-        <MapContainer>
+        <MapContainer className={style.map} center={position} zoom={14} scrollWheelZoom={true}
+        >f
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
 
             <Marker position={position}>
-            <Popup>
-                Skytrain. <br/> Waterfront.
-            </Popup>
-            
+                <Popup>
+                    Skytrain: <br />Waterfront Station
+                </Popup>
 
             </Marker>
         </MapContainer>
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
+import Map from '../components/Map'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,8 +15,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <main className={styles.main}>
-       
+       <Map></Map>
        
       </main>
     </>
